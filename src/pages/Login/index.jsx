da833b4cea236222550018c604e5964e8d54c496
EddyeBoy27 <lipe0503@gmail.com>
2020-04-15 18:54:59 -0300

    Finalizado dataTestId

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,10 +1,10 @@
 import React, { useContext, useState } from 'react';
-import RecipeAppContext from '../../context/Context';
-import { Link } from 'react-router-dom';
+// import RecipeAppContext from '../../context/Context';
+// import { Link } from 'react-router-dom';
 import './style/style.css';
 
 const Login = () => {
-  const context = useContext(RecipeAppContext);
+  // const context = useContext(RecipeAppContext);
   const [email, setEmail] = useState(false);
   const [pass, setPass] = useState(false);
   const resultadoEmail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -37,16 +37,30 @@ const Login = () => {
     <div className="containerLogin">
       <h1>Login</h1>
       <form className="containerForm">
-        <input type="text" placeholder="EMAIL" name="email" required onChange={(e) => onChangeHandleEmail(e.target)}/>
+        <input
+          type="text"
+          placeholder="EMAIL"
+          data-testid="email-input"
+          name="email"
+          required
+          onChange={(e) => onChangeHandleEmail(e.target)}
+        />
         <input
           type="password"
           placeholder="SENHA"
           name="password"
+          data-testid="password-input"
           required minLength="8"
           onChange={(e) => onChangeHandlePass(e.target)}
         />
         {/* <Link to="/comidas"> */}
-        {email && pass ? <button onClick={(e) => handleSubmit(e)}>Entrar</button> : <button disabled>Entrar</button>}
+        {email && pass
+        ? <button
+            data-testid="login-submit-btn"
+            onClick={(e) => handleSubmit(e)}>
+            Entrar
+          </button>
+        : <button data-testid="login-submit-btn" disabled>Entrar</button>}
           
         {/* </Link> */}
       </form>
