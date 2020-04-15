dd13e7626f66cf2f7e168fc79d8c55472fbc9afb
phtf92 <phtf92@gmail.com>
2020-04-15 19:48:43 -0300

    changing footer icons

--- src/components-global/Footer.js
+++ src/components-global/Footer.js
@@ -7,8 +7,8 @@ const Footer = () => (
     <div className="phantom-footer" />
     <div className="footer-list-container">
       <ul className="flex-footer-container">
-        <Link to="/bebidas"><li><span className="material-icons">local_drink</span></li></Link>
-        <Link to="/explorar"><li><span className="material-icons">local_dining</span></li></Link>
+        <Link to="/bebidas"><li><span className="material-icons">local_bar</span></li></Link>
+        <Link to="/explorar"><li><span className="material-icons">explore</span></li></Link>
         <Link to="/comidas"><li><span className="material-icons">local_pizza</span></li></Link>
       </ul>
     </div>
