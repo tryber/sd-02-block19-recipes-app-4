ee47cebf87f81ad3d47ed6dc794166d2f5298b19
Andrey <andreytsuzuki@gmail.com>
2020-04-17 22:47:47 -0300

    ups

--- src/pages/ItemId/components/Generics.jsx
+++ src/pages/ItemId/components/Generics.jsx
@@ -12,35 +12,55 @@ const recomended = () => (
   </div>
 );
 
-const ingredientsList = (ingridients) => (
+const ingredientsList = (checks) => (
   <ul>
-    {ingridients.map((item, index) => (
-      <li key={`${item[0]}${item[1]}`}>
-        <span data-testid={`${index}-ingredient-name`}>{item[0]}</span>
-        <span data-testid={`${index}-ingredient-measure`}> - {item[1]}</span>
+    {checks.map((item, index) => (
+      <li key={`${item.ingridient}${item.measure}`}>
+        <span data-testid={`${index}-ingredient-name`}>{item.ingridient}</span>
+        <span data-testid={`${index}-ingredient-measure`}> - {item.measure}</span>
       </li>
     ))}
   </ul>
 );
 
-const ingredientsCheckbox = (ingridients) => (
+const allChecked = (checks) => {
+  return !checks.every((obj) => obj.check);
+};
+
+const changeHandle = (e, checks, setChecks, item, setAllChecked) => {
+  const { name, checked } = e.target;
+  document.querySelector(`#${item.ingridient}${item.measure}`.replace(/\s/g, ''))
+    .style.textDecoration = (checked) ? 'line-through' : 'none';
+  checks[name].check = checked;
+  setChecks(() => checks);
+  setAllChecked(allChecked(checks));
+};
+
+const ingredientsCheckbox = (checks, setChecks, setAllChecked) => (
   <div>
-    {ingridients.map((item) => (
-      <div className="checkbox" key={item}>
-        <input type="checkbox" key={item} name={item} />
-        <p>{item}</p>
+    {checks.map((item, index) => (
+      <div className="checkbox" key={`${item.ingridient}${item.measure}`}>
+        <input
+          type="checkbox"
+          name={index}
+          onChange={(e) => changeHandle(e, checks, setChecks, item, setAllChecked)}
+        />
+        <label id={`${item.ingridient}${item.measure}`.replace(/\s/g, '')}>
+          <span data-testid={`${index}-ingredient-name`}>{item.ingridient}</span>
+          <span data-testid={`${index}-ingredient-measure`}> - {item.measure}</span>
+        </label>
       </div>
     ))}
   </div>
 );
 
-const ingredients = (ingridients, making) => (
+const ingredients = (checks, making, setChecks, setAllChecked) => (
   <div className="ingredients">
     <p className="subtitle">Ingredients</p>
     <div className="box">
       {(making) ?
-        ingredientsCheckbox(ingridients) :
-        ingredientsList(ingridients)}
+        ingredientsCheckbox(checks, setChecks, setAllChecked) :
+        ingredientsList(checks)}
     </div>
   </div>
 );
@@ -99,14 +119,14 @@ const header = (strFood, data, favorite, setFavorite) => (
           share
         </span>
       </button>
-      {<button type="button" data-testid="favorite-btn">
+      <button type="button" data-testid="favorite-btn">
         <span
           className="material-icons"
           onClick={() => btnFavorite(data, setFavorite)}
         >
           {(favorite) ? 'favorite' : 'favorite_border'}
         </span>
-      </button>}
+      </button>
     </div>
   </div>
 );
@@ -121,12 +141,13 @@ const handleBtnStart = (data, type, history) => {
   history.push(`/receitas/${type}/${id}/making`);
 };
 
-const btnMaking = (history) => (
+const btnMaking = (history, allChecked) => (
   <button
     type="button"
     className="init"
     data-testid="start-recipe-btn"
     onClick={() => handleBtnMaking(history)}
+    disabled={allChecked}
   >
     Finalizar receita
   </button>
@@ -143,28 +164,43 @@ const btnStart = (data, type, history) => (
   </button>
 );
 
-const buttonSwitch = (making, data, type, history) => {
+const buttonSwitch = (making, data, type, history, checks) => {
   if (making) {
-    return btnMaking(history);
+    return btnMaking(history, checks);
   }
   return btnStart(data, type, history);
 };
 
+const initChecks = (ingridients) => {
+  const arr = [];
+  ingridients.forEach((item) => {
+    const obj = {};
+    obj['ingridient'] = item[0];
+    obj['measure'] = item[1];
+    obj['check'] = false;
+    arr.push(obj);
+  });
+  return arr;
+};
+
 function Generics(props) {
   const { data, making, type, history } = props;
-  const [favorite, setFavorite] = useState(initFavoriteParam(data));
   const { strFood, strThumb, strCategory, strInstructions, strYoutube, ingridients } = data;
+  const [checks, setChecks] = useState(initChecks(ingridients));
+  const [allChecked, setAllChecked] = useState(true);
+  const [favorite, setFavorite] = useState(initFavoriteParam(data));
+
   return (
     <React.Fragment>
       <img src={strThumb} data-testid="recipe-photo" alt="" />
       <div className="main">
         {header(strFood, data, favorite, setFavorite)}
         <p className="type">{strCategory}</p>
-        {ingredients(ingridients, making)}
+        {ingredients(checks, making, setChecks, setAllChecked)}
         {instruction(strInstructions)}
         {(making) ? <div /> : (video(strYoutube))}
         {(making) ? <div /> : recomended()}
-        {buttonSwitch(making, data, type, history)}
+        {buttonSwitch(making, data, type, history, allChecked)}
       </div>
     </React.Fragment>
   );
