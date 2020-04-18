6955e50a7dc5a85961eb1fe8fbf0cb903b8de205
phtf92 <phtf92@gmail.com>
2020-04-18 20:05:08 -0300

    CC

--- src/components-global/renderCards/index.jsx
+++ src/components-global/renderCards/index.jsx
@@ -7,7 +7,7 @@ import './style/index.css';
 
 const changeRoute = (history, minPrefix, id) => {
   history.push(`/receitas/${minPrefix}/${id}`);
-}
+};
 
 const renderCardsFunction = (adjustedData, prefix) => (
   <div>
@@ -20,22 +20,20 @@ const renderCardsFunction = (adjustedData, prefix) => (
 );
 
 const RenderCards = () => {
-  const { results: [data], dataBase: [db], isOnSearchBar, } = useContext(context);
+  const { results: [data], dataBase: [db], isOnSearchBar } = useContext(context);
   const history = useHistory();
   const prefix = db === 'themealdb' ? 'Meal' : 'Drink';
   const minPrefix = db === 'themealdb' ? 'meals' : 'drinks';
   let adjustedData = data.meals || data.drinks;
   if (data.length === 12) {
     adjustedData = data.map(({ [minPrefix]: [returnedData] }) => returnedData);
-    console.log(adjustedData);
   }
-  if (!adjustedData) return <Loading />;
-  console.log(adjustedData[0]);
   return (
     <div className="container-all-recipes">
-      {isOnSearchBar && adjustedData.length === 1
+      {!adjustedData && <Loading />}
+      {isOnSearchBar && adjustedData && adjustedData.length === 1
         && changeRoute(history, minPrefix, adjustedData[0][`id${prefix}`])}
-      {renderCardsFunction(adjustedData, prefix)}
+      {adjustedData && renderCardsFunction(adjustedData, prefix)}
     </div>
   );
 };
