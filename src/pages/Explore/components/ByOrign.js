c4f2badd29b1737fac76c1dea564e5a5471262c8
phtf92 <phtf92@gmail.com>
2020-04-20 20:23:27 -0300

    ingredients file

--- src/pages/Explore/components/ByOrign.js
+++ src/pages/Explore/components/ByOrign.js
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState, useContext } from 'react';
+import context from '../../../context/Context';
 
 const ByOrign = () => {
-  console.log('byOrign');
+  const [areas, setAreas] = useState([]);
+  const [selectedArea, setSelectedArea] = useState([]);
+  const [recipesByContry, setRecipesByContry] = useState([]);
+  const { fetchRecipe } = useContext(context)
+
+  const adjustSelectorArea = (response) => {
+    setAreas(() => {
+      return [{ strArea: 'All areas' }, ...response.meals];
+    })
+  }
+
+  useEffect(() => {
+    fetchRecipe('themealdb', 'list.php?a=list', adjustSelectorArea)
+  }, []);
+
+  useEffect(() => {
+    fetchRecipe('themealdb', `filter.php?a=${selectedArea}`, setRecipesByContry)
+  }, [selectedArea]);
+
+  console.log(recipesByContry.meals)
+
   return (
     <div>
-      <p>Origem</p>
+      <select onChange={(e) => setSelectedArea(e.target.value)}>
+        {areas.map(({ strArea }) => {
+          return <option value={strArea} key={strArea}>{strArea}</option>
+        })}
+      </select>
+      {!recipesByContry.meals && <div>Nenhum resultado</div>}
+      {recipesByContry.meals && recipesByContry.meals.map(recipe => {
+        return <p key={recipe.strMeal}>{recipe.strMeal}</p>
+      })}
     </div>
   );
 };
