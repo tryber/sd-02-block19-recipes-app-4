e443d10423db7e0105a4cd62dd2e07c3aa883e18
EddyeBoy27 <lipe0503@gmail.com>
2020-04-30 10:33:09 -0300

    Finalizado Testes DoneRecipes

--- src/FavoriteRecipes.test.js
+++ src/FavoriteRecipes.test.js
@@ -29,23 +29,25 @@ test('Testing Static Items on FavoritesRecipes', async () => {
   const lStorage = JSON.parse(localStorage.getItem('favorite-recipes'));
   expect(lStorage).toEqual([{id:"52878",category:"Beef",image:"https://www.themealdb.com/images/media/meals/wrssvt1511556563.jpg",type:"comida"}]);
   localStorage.setItem('user', JSON.stringify({"email":"Lipe_Lim@hotmail.com"}));
-  
+
   history.push('/perfil');
-  
+
   const favRecipesBtn = getByTestId('profile-favorite-btn');
   expect(favRecipesBtn).toBeInTheDocument();
   fireEvent.click(favRecipesBtn);
   expect(getByText(/Receitas favoritas/i)).toBeInTheDocument();
-  
+
   await waitForDomChange();
-  
+
   const location = getByText(/British/i);
   const subtitle = getAllByText(/Beef/i);
-  const imageSrc = document.querySelector('img').getAttribute('src');
-  expect(imageSrc).toEqual('https://www.themealdb.com/images/media/meals/wrssvt1511556563.jpg');
+  const imageSrc = document.querySelector('img');
+  expect(imageSrc.src).toEqual('https://www.themealdb.com/images/media/meals/wrssvt1511556563.jpg');
+  expect(imageSrc).toBeInTheDocument();
   expect(subtitle[0]).toBeInTheDocument();
   expect(subtitle[0].innerHTML).toEqual('Beef');
   expect(subtitle[1]).toBeInTheDocument();
   expect(subtitle[1].innerHTML).toEqual('Beef and Oyster pie');
   expect(location).toBeInTheDocument();
 })
+
