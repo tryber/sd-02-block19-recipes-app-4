4d9121ebf5596dc9237adad2f5850a8262632e77
phtf92 <phtf92@gmail.com>
2020-05-07 13:44:37 -0300

    CC

--- src/components-global/BadRoute/BadRoute.js
+++ src/components-global/BadRoute/BadRoute.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const BadRoute = () => (
-    <div>
-      <div>Página não encontrada</div>
-      <Link to='/explorar/bebidas'><button>Voltar</button></Link>
-    </div>
-  );
+  <div>
+    <div>Página não encontrada</div>
+    <Link to="/explorar/bebidas"><button>Voltar</button></Link>
+  </div>
+);
 
 export default BadRoute;
